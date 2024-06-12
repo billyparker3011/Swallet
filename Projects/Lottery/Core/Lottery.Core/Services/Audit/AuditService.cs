2dc6875b9fc7da714fb23753c5db26990efc2a8f
DESKTOP-3OEV9MB\phu son <billyparker3011@gmail.com>
2024-06-13 00:00:59 +0700

    cherry pick implement get audits by type

--- Projects/Lottery/Core/Lottery.Core/Services/Audit/AuditService.cs
+++ Projects/Lottery/Core/Lottery.Core/Services/Audit/AuditService.cs
@@ -1,7 +1,12 @@
-﻿using HnMicro.Framework.Services;
+﻿using HnMicro.Core.Helpers;
+using HnMicro.Framework.Exceptions;
+using HnMicro.Framework.Services;
 using Lottery.Core.Contexts;
 using Lottery.Core.Dtos.Audit;
+using Lottery.Core.Enums;
 using Lottery.Core.Helpers;
+using Lottery.Core.Models.Audit;
+using Lottery.Core.Repositories.Agent;
 using Lottery.Core.Repositories.Audit;
 using Lottery.Core.UnitOfWorks;
 using Lottery.Data.Entities;
@@ -21,6 +26,66 @@ namespace Lottery.Core.Services.Audit
             ILotteryUow lotteryUow) 
             : base(logger, serviceProvider, configuration, clockService, clientContext, lotteryUow)
         {
+        }
+
+        public async Task<GetAuditsByTypeResult> GetAuditsByType(GetAuditsByTypeModel query)
+        {
+            //Init repos
+            var agentRepos = LotteryUow.GetRepository<IAgentRepository>();
+            var auditRepos = LotteryUow.GetRepository<IAuditRepository>();
+            var targetAgentId = ClientContext.Agent.ParentId != 0 ? ClientContext.Agent.ParentId : ClientContext.Agent.AgentId;
+            var clientAgent = await agentRepos.FindByIdAsync(targetAgentId) ?? throw new NotFoundException();
+
+            IQueryable<Data.Entities.Audit> auditQuery = auditRepos.FindQueryBy(x => x.Type == query.Type && x.CreatedAt >= query.DateFrom.Date && x.CreatedAt <= query.DateTo.AddDays(1).AddTicks(-1));
+            if (clientAgent.RoleId != Role.Company.ToInt())
+            {
+                switch (clientAgent.RoleId)
+                {
+                    case (int)Role.Supermaster:
+                        auditQuery = auditQuery.Where(x => x.SupermasterId == clientAgent.AgentId);
+                        break;
+                    case (int)Role.Master:
+                        auditQuery = auditQuery.Where(x => x.MasterId == clientAgent.AgentId);
+                        break;
+                    case (int)Role.Agent:
+                        auditQuery = auditQuery.Where(x => x.AgentId == clientAgent.AgentId);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(query.SearchTerm))
+            {
+                auditQuery = auditQuery.Where(x =>
+                        x.UserName.Contains(query.SearchTerm) ||
+                        x.LastName.Contains(query.SearchTerm) ||
+                        x.FirstName.Contains(query.SearchTerm));
+            }
+
+            var result = await auditRepos.PagingByAsync(auditQuery, query.PageIndex, query.PageSize);
+            return new GetAuditsByTypeResult
+            {
+                Audits = result.Items.Select(x => new AuditDto
+                {
+                    AuditId = x.AuditId,
+                    Action = x.Action,
+                    AuditData = x.AuditData,
+                    CreatedAt = x.CreatedAt,
+                    EditedBy = x.EdittedBy,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Type = x.Type,
+                    Username = x.UserName
+                }),
+                Metadata = new HnMicro.Framework.Responses.ApiResponseMetadata
+                {
+                    NoOfPages = result.Metadata.NoOfPages,
+                    NoOfRows = result.Metadata.NoOfRows,
+                    NoOfRowsPerPage = result.Metadata.NoOfRowsPerPage,
+                    Page = result.Metadata.Page
+                }
+            };
         }
 
         public async Task SaveAuditData(AuditParams auditParams)
--- Projects/Lottery/Core/Lottery.Core/Services/Audit/IAuditService.cs
+++ Projects/Lottery/Core/Lottery.Core/Services/Audit/IAuditService.cs
@@ -1,10 +1,12 @@
 ﻿using HnMicro.Core.Scopes;
 using Lottery.Core.Dtos.Audit;
+using Lottery.Core.Models.Audit;
 
 namespace Lottery.Core.Services.Audit
 {
     public interface IAuditService : IScopedDependency
     {
         Task SaveAuditData(AuditParams auditParams);
+        Task<GetAuditsByTypeResult> GetAuditsByType(GetAuditsByTypeModel query);
     }
 }
