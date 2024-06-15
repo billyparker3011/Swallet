aca0a2f0ce1ecfc1bd2c221c28ccf2a04233472d
Henry Nguyen <thtgroup2003@gmail.com>
2024-06-15 11:17:12 +0700

    Update database.

---
+++ Projects/Lottery/Core/Lottery.Data/Migrations/20240615041541_AddMoreColInAudit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lottery.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreColInAudit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuditSettingDatas",
+                table: "Audits",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuditSettingDatas",
+                table: "Audits");
+        }
+    }
+}
