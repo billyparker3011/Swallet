2b42c7fef9c736ba3cce76de104daf74b0f73960
Henry Nguyen <thtgroup2003@gmail.com>
2024-06-28 22:41:07 +0700

    Update code.

--- Projects/Lottery/Core/Lottery.Core/Services/Setting/IProcessTicketSettingService.cs
+++ Projects/Lottery/Core/Lottery.Core/Services/Setting/IProcessTicketSettingService.cs
@@ -5,7 +5,10 @@ namespace Lottery.Core.Services.Setting
 {
     public interface IProcessTicketSettingService : IScopedDependency
     {
+        Task<ScanWaitingTicketSettingModel> GetScanWaitingTicketSetting();
         Task UpdateScanWaitingTicketSetting(ScanWaitingTicketSettingModel model);
+
+        Task<ValidationPrizeSettingModel> GetValidationPrizeSetting(int betKindId);
         Task UpdateValidationPrizeSetting(ValidationPrizeSettingModel model);
     }
 }
