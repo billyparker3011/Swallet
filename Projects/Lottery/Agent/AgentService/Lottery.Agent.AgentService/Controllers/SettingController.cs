47f868f66a87718e20de8683688bf70bb63f34c2
Henry Nguyen <thtgroup2003@gmail.com>
2024-07-07 10:10:07 +0700

    Create balance table setting.

--- Projects/Lottery/Agent/AgentService/Lottery.Agent.AgentService/Controllers/SettingController.cs
+++ Projects/Lottery/Agent/AgentService/Lottery.Agent.AgentService/Controllers/SettingController.cs
@@ -1,5 +1,7 @@
 ﻿using HnMicro.Framework.Controllers;
+using Lottery.Agent.AgentService.Requests.Setting.BetKind;
 using Lottery.Agent.AgentService.Requests.Setting.ProcessTicket;
+using Lottery.Core.Models.Setting.BetKind;
 using Lottery.Core.Models.Setting.ProcessTicket;
 using Lottery.Core.Services.Setting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +12,12 @@ namespace Lottery.Agent.AgentService.Controllers
     public class SettingController : HnControllerBase
     {
         private readonly IProcessTicketSettingService _processTicketSettingService;
+        private readonly IBalanceTableSettingService _balanceTableSettingService;
 
-        public SettingController(IProcessTicketSettingService processTicketSettingService)
+        public SettingController(IProcessTicketSettingService processTicketSettingService, IBalanceTableSettingService balanceTableSettingService)
         {
             _processTicketSettingService = processTicketSettingService;
+            _balanceTableSettingService = balanceTableSettingService;
         }
 
         #region Process Ticket Setting
@@ -57,6 +61,36 @@ namespace Lottery.Agent.AgentService.Controllers
         public async Task<IActionResult> GetValidationPrizeSetting([FromRoute] int betKindId)
         {
             return Ok(await _processTicketSettingService.GetValidationPrizeSetting(betKindId));
+        }
+        #endregion
+
+        #region Balance Table Setting
+        [HttpPut("balance-table/{betKindId:int}")]
+        public async Task<IActionResult> CreateBalanceTableSetting([FromRoute] int betKindId, [FromBody] BalanceTableRequest request)
+        {
+            await _balanceTableSettingService.CreateBalanceTableSetting(betKindId, new BalanceTableModel
+            {
+                ByNumbers = new BalanceTableNumberDetailModel
+                {
+                    Numbers = request.ByNumbers.Numbers,
+                    RateValues = request.ByNumbers.RateValues.Select(f => new BalanceTableRateModel
+                    {
+                        From = f.From,
+                        To = f.To,
+                        RateValue = f.RateValue
+                    }).ToList()
+                },
+                ForCommon = new BalanceTableCommonDetailModel
+                {
+                    RateValues = request.ForCommon.RateValues.Select(f => new BalanceTableRateModel
+                    {
+                        From = f.From,
+                        To = f.To,
+                        RateValue = f.RateValue
+                    }).ToList()
+                }
+            });
+            return Ok();
         }
         #endregion
     }
--- Projects/Lottery/Core/Lottery.Core/Services/Setting/IBalanceTableSettingService.cs
+++ Projects/Lottery/Core/Lottery.Core/Services/Setting/IBalanceTableSettingService.cs
@@ -1,9 +1,11 @@
 ﻿using HnMicro.Core.Scopes;
+using Lottery.Core.Models.Setting.BetKind;
 
 namespace Lottery.Core.Services.Setting
 {
     public interface IBalanceTableSettingService : IScopedDependency
     {
         string CreateBalanceTableKey(int betKindId);
+        Task CreateBalanceTableSetting(int betKindId, BalanceTableModel model);
     }
 }
