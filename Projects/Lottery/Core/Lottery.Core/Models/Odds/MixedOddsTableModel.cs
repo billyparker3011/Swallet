b452a78eb75d77292b65c4d99efb5cfba8006342
Henry Nguyen <thtgroup2003@gmail.com>
2024-07-11 22:37:27 +0700

    Update mixed odds table.

--- Projects/Lottery/Core/Lottery.Core/Models/Odds/MixedOddsTableModel.cs
+++ Projects/Lottery/Core/Lottery.Core/Models/Odds/MixedOddsTableModel.cs
@@ -5,6 +5,7 @@ namespace Lottery.Core.Models.Odds
     public class MixedOddsTableModel
     {
         public MatchModel Match { get; set; }
-        public Dictionary<int, List<OddsTableDetailModel>> Odds { get; set; }
+        //public Dictionary<int, List<OddsTableDetailModel>> Odds { get; set; }
+        public Dictionary<int, string> BetKinds { get; set; }
     }
 }
