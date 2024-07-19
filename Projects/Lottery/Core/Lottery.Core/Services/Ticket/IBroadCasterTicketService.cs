2cb9f2c51eba1baeea2d4a27ed733885f1d593e1
DESKTOP-3OEV9MB\phu son <billyparker3011@gmail.com>
2024-07-19 22:43:09 +0700

    implement view detail broadcaster outstanding

---
+++ Projects/Lottery/Core/Lottery.Core/Services/Ticket/IBroadCasterTicketService.cs
@@ -0,0 +1,9 @@
+﻿using HnMicro.Core.Scopes;
+using Lottery.Core.Models.Ticket;
+
+namespace Lottery.Core.Services.Ticket;
+
+public interface IBroadCasterTicketService : IScopedDependency
+{
+    Task<List<TicketDetailModel>> GetBroadCasterOuts(long betkindId);
+}
