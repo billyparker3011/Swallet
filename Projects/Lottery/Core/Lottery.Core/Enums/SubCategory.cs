745cb11bb7fcf1c98678304a7a5754d6636e9dfb
Henry Nguyen <thtgroup2003@gmail.com>
2024-08-06 16:13:14 +0000

    Merged PR 323: Update code.

--- Projects/Lottery/Core/Lottery.Core/Enums/SubCategory.cs
+++ Projects/Lottery/Core/Lottery.Core/Enums/SubCategory.cs
@@ -14,44 +14,44 @@ namespace Lottery.Core.Enums
 
         #region For Second Northern
         [EnumSubCategoryDescription("2D", 1, Category.SecondNorthern, BetKind.None, new[] { BetKind.SecondNorthern_Northern_2DDau, BetKind.SecondNorthern_Northern_2DDuoi })]
-        D2 = 3,
+        D2 = 2,
 
         [EnumSubCategoryDescription("3D", 2, Category.SecondNorthern, BetKind.None, new[] { BetKind.SecondNorthern_Northern_3DDau, BetKind.SecondNorthern_Northern_3DDuoi, BetKind.SecondNorthern_Northern_3D23Lo })]
-        D3 = 4,
+        D3 = 3,
 
         [EnumSubCategoryDescription("4D", 2, Category.SecondNorthern, BetKind.None, new[] { BetKind.SecondNorthern_Northern_4DDuoi, BetKind.SecondNorthern_Northern_4D20Lo })]
-        D4 = 5,
+        D4 = 4,
         #endregion
 
         #region For Central
         [EnumSubCategoryDescription("2D", 1, Category.Central, BetKind.None, new[] { BetKind.Central_2DDau, BetKind.Central_2DDuoi, BetKind.Central_2D18Lo, BetKind.Central_2D18LoDau, BetKind.Central_2D7Lo })]
-        CentralD2 = 6,
+        CentralD2 = 5,
 
         [EnumSubCategoryDescription("3D", 2, Category.Central, BetKind.None, new[] { BetKind.Central_3DDau, BetKind.Central_3DDuoi, BetKind.Central_3D17Lo, BetKind.Central_3D7Lo })]
-        CentralD3 = 7,
+        CentralD3 = 6,
 
         [EnumSubCategoryDescription("4D", 3, Category.Central, BetKind.None, new[] { BetKind.Central_4DDuoi, BetKind.Central_4D16Lo })]
-        CentralD4 = 8,
+        CentralD4 = 7,
 
         [EnumSubCategoryDescription("Xiên", 4, Category.Central, BetKind.Central_LoXien)]
-        CentralMixed = 9,
+        CentralMixed = 8,
 
         [EnumSubCategoryDescription("Xiên 18A+B", 5, Category.Central, BetKind.Central_Mixed_LoXien)]
-        CentralMixed18AB = 10,
+        CentralMixed18AB = 9,
         #endregion
 
         #region For Southern
         [EnumSubCategoryDescription("2D", 1, Category.Southern, BetKind.None, new[] { BetKind.Southern_2DDau, BetKind.Southern_2DDuoi, BetKind.Southern_2D18Lo, BetKind.Southern_2D18LoDau, BetKind.Southern_2D7Lo })]
-        SouthernD2 = 11,
+        SouthernD2 = 10,
 
         [EnumSubCategoryDescription("3D", 2, Category.Southern, BetKind.None, new[] { BetKind.Southern_3DDau, BetKind.Southern_3DDuoi, BetKind.Southern_3D17Lo, BetKind.Southern_3D7Lo })]
-        SouthernD3 = 12,
+        SouthernD3 = 11,
 
         [EnumSubCategoryDescription("4D", 3, Category.Southern, BetKind.None, new[] { BetKind.Southern_4DDuoi, BetKind.Southern_4D16Lo })]
-        SouthernD4 = 13,
+        SouthernD4 = 12,
 
         [EnumSubCategoryDescription("Xiên", 4, Category.Southern, BetKind.Southern_LoXien)]
-        SouthernMixed = 14,
+        SouthernMixed = 13,
 
         [EnumSubCategoryDescription("Xiên 18A+B", 5, Category.Southern, BetKind.Southern_Mixed_LoXien)]
         SouthernMixed18AB = 14
