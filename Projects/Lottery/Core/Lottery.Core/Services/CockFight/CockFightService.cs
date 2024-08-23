7996fbac1b50302b416f84eaf7b78e8dffa0880a
Henry Nguyen <thtgroup2003@gmail.com>
2024-08-23 09:12:28 +0000

    Merged PR 498: Update code.

--- Projects/Lottery/Core/Lottery.Core/Services/CockFight/CockFightService.cs
+++ Projects/Lottery/Core/Lottery.Core/Services/CockFight/CockFightService.cs
@@ -7,6 +7,7 @@ using Lottery.Core.Dtos.CockFight;
 using Lottery.Core.Enums.Partner;
 using Lottery.Core.Helpers;
 using Lottery.Core.Models.CockFight.GetBalance;
+using Lottery.Core.Partners.Helpers;
 using Lottery.Core.Partners.Models.Ga28;
 using Lottery.Core.Partners.Publish;
 using Lottery.Core.Repositories.BookiesSetting;
@@ -245,56 +246,56 @@ namespace Lottery.Core.Services.CockFight
             var balance = await _singleWalletService.GetBalanceForGa28(player.PlayerId);
             if (ticketAmount > balance) throw new HnMicroException();
 
-            //var betKindRepository = LotteryUow.GetRepository<ICockFightBetKindRepository>();
-            //var betKind = await betKindRepository.FindQueryBy(f => true).FirstOrDefaultAsync();
-            //if (betKind == null) return;
+            var betKindRepository = LotteryUow.GetRepository<ICockFightBetKindRepository>();
+            var betKind = await betKindRepository.FindQueryBy(f => true).FirstOrDefaultAsync();
+            if (betKind == null) return;
 
-            ////  TODO Need to storage Match & Fight
-            //var cockFightTicketRepository = LotteryUow.GetRepository<ICockFightTicketRepository>();
-            //var ticket = await cockFightTicketRepository.FindBySId(model.Ticket.Sid);
-            //if (ticket == null)
-            //{
-            //    ticket = new CockFightTicket
-            //    {
-            //        PlayerId = player.PlayerId,
-            //        AgentId = player.AgentId,
-            //        MasterId = player.MasterId,
-            //        SupermasterId = player.SupermasterId,
-            //        BetKindId = betKind.Id,
-            //        AnteAmount = string.IsNullOrEmpty(model.Ticket.AnteAmount) || !decimal.TryParse(model.Ticket.AnteAmount, out decimal anteAmount) ? null : anteAmount,
-            //        ArenaCode = model.Ticket.ArenaCode,
-            //        BetAmount = string.IsNullOrEmpty(model.Ticket.BetAmount) || !decimal.TryParse(model.Ticket.BetAmount, out decimal betAmount) ? null : betAmount,
-            //        CurrencyCode = model.Ticket.CurrencyCode,
-            //        FightNumber = model.Ticket.FightNumber,
-            //        MatchDayCode = model.Ticket.MatchDayCode,
-            //        Odds = string.IsNullOrEmpty(model.Ticket.Odds) || !decimal.TryParse(model.Ticket.Odds, out decimal odds) ? null : odds,
-            //        Result = model.Ticket.Result.ToTicketResult(),
-            //        Selection = model.Ticket.Selection,
-            //        SettledDateTime = model.Ticket.SettledDateTime,
-            //        Sid = model.Ticket.Sid,
-            //        Status = model.Ticket.Status,
-            //        TicketAmount = ticketAmount,
-            //        WinlossAmount = string.IsNullOrEmpty(model.Ticket.WinLossAmount) || !decimal.TryParse(model.Ticket.WinLossAmount, out decimal winlossAmount) ? null : winlossAmount,
-            //        IpAddress = model.Ticket.IpAddress,
-            //        UserAgent = model.Ticket.UserAgent,
-            //        TicketCreatedDate = model.Ticket.CreatedDateTime,
-            //        TicketModifiedDate = model.Ticket.ModifiedDateTime,
-            //        ValidStake = string.IsNullOrEmpty(model.Ticket.ValidStake) || !decimal.TryParse(model.Ticket.ValidStake, out decimal validStake) ? null : validStake,
-            //        OddType = model.Ticket.OddsType
-            //    };
-            //    cockFightTicketRepository.Add(ticket);
-            //}
-            //else
-            //{
-            //    ticket.Result = model.Ticket.Result.ToTicketResult();
-            //    ticket.SettledDateTime = model.Ticket.SettledDateTime;
-            //    ticket.Status = model.Ticket.Status;
-            //    ticket.WinlossAmount = string.IsNullOrEmpty(model.Ticket.WinLossAmount) || !decimal.TryParse(model.Ticket.WinLossAmount, out decimal winlossAmount) ? null : winlossAmount;
-            //    ticket.TicketModifiedDate = model.Ticket.ModifiedDateTime;
-            //    cockFightTicketRepository.Update(ticket);
-            //}
+            //  TODO Need to storage Match & Fight
+            var cockFightTicketRepository = LotteryUow.GetRepository<ICockFightTicketRepository>();
+            var ticket = await cockFightTicketRepository.FindBySId(model.Ticket.Sid);
+            if (ticket == null)
+            {
+                ticket = new CockFightTicket
+                {
+                    PlayerId = player.PlayerId,
+                    AgentId = player.AgentId,
+                    MasterId = player.MasterId,
+                    SupermasterId = player.SupermasterId,
+                    BetKindId = betKind.Id,
+                    AnteAmount = string.IsNullOrEmpty(model.Ticket.AnteAmount) || !decimal.TryParse(model.Ticket.AnteAmount, out decimal anteAmount) ? null : anteAmount,
+                    ArenaCode = model.Ticket.ArenaCode,
+                    BetAmount = string.IsNullOrEmpty(model.Ticket.BetAmount) || !decimal.TryParse(model.Ticket.BetAmount, out decimal betAmount) ? null : betAmount,
+                    CurrencyCode = model.Ticket.CurrencyCode,
+                    FightNumber = model.Ticket.FightNumber,
+                    MatchDayCode = model.Ticket.MatchDayCode,
+                    Odds = string.IsNullOrEmpty(model.Ticket.Odds) || !decimal.TryParse(model.Ticket.Odds, out decimal odds) ? null : odds,
+                    Result = model.Ticket.Result.ToTicketResult(),
+                    Selection = model.Ticket.Selection,
+                    SettledDateTime = model.Ticket.SettledDateTime,
+                    Sid = model.Ticket.Sid,
+                    Status = model.Ticket.Status,
+                    TicketAmount = ticketAmount,
+                    WinlossAmount = string.IsNullOrEmpty(model.Ticket.WinLossAmount) || !decimal.TryParse(model.Ticket.WinLossAmount, out decimal winlossAmount) ? null : winlossAmount,
+                    IpAddress = model.Ticket.IpAddress,
+                    UserAgent = model.Ticket.UserAgent,
+                    TicketCreatedDate = model.Ticket.CreatedDateTime,
+                    TicketModifiedDate = model.Ticket.ModifiedDateTime,
+                    ValidStake = string.IsNullOrEmpty(model.Ticket.ValidStake) || !decimal.TryParse(model.Ticket.ValidStake, out decimal validStake) ? null : validStake,
+                    OddType = model.Ticket.OddsType
+                };
+                cockFightTicketRepository.Add(ticket);
+            }
+            else
+            {
+                ticket.Result = model.Ticket.Result.ToTicketResult();
+                ticket.SettledDateTime = model.Ticket.SettledDateTime;
+                ticket.Status = model.Ticket.Status;
+                ticket.WinlossAmount = string.IsNullOrEmpty(model.Ticket.WinLossAmount) || !decimal.TryParse(model.Ticket.WinLossAmount, out decimal winlossAmount) ? null : winlossAmount;
+                ticket.TicketModifiedDate = model.Ticket.ModifiedDateTime;
+                cockFightTicketRepository.Update(ticket);
+            }
 
-            //await LotteryUow.SaveChangesAsync();
+            await LotteryUow.SaveChangesAsync();
         }
     }
 }
