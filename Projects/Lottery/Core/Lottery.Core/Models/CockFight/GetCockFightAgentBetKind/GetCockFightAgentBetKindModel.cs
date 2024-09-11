736252bed24e168e1784d373e111c0d72fec13bc
Billy Parker <billyparker3011@gmail.com>
2024-09-11 01:06:34 +0000

    Merged PR 625: implement manage master cock fight bet kind
    implement manage master cock fight bet kind

---
+++ Projects/Lottery/Core/Lottery.Core/Models/CockFight/GetCockFightAgentBetKind/GetCockFightAgentBetKindModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Lottery.Core.Models.CockFight.GetCockFightAgentBetKind
+{
+    public class GetCockFightAgentBetKindModel
+    {
+        public string Name { get; set; }
+        public bool? Enabled { get; set; }
+    }
+}
