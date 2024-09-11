3cc1a5458564691529c5258d4201258c00713373
Henry Nguyen <thtgroup2003@gmail.com>
2024-09-11 07:11:28 +0000

    Merged PR 633: Update code.

---
+++ Projects/SWallet/Core/SWallet.Core/Helpers/WebApplicationBuilderHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace SWallet.Core.Helpers
+{
+    public static class WebApplicationBuilderHelper
+    {
+        public static void BuildSWalletService(this WebApplicationBuilder builder)
+        {
+            //builder.AddSqlServer<SWalletContext>();
+            //builder.Services.AddScoped<ISWalletUow, SWalletUow>();
+        }
+    }
+}
---
+++ Projects/SWallet/Flow/SWallet.CustomerService/Helpers/CustomerServiceWebApplicationBuilderHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace SWallet.CustomerService.Helpers
+{
+    public static class CustomerServiceWebApplicationBuilderHelper
+    {
+        public static void BuildSWalletCustomerService(this WebApplicationBuilder builder)
+        {
+
+        }
+    }
+}
